754dd52e9b69468ddd4c71580687ff84b133958f
Vis'Eyth <viseyth_dev@protonmail.com>
2019-04-21 23:20:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## UnmoddedClients - BepInEx plugin for allowing unmodded clients to join
+
+##### PLEASE NOTE: This is very experimental. Please report any bugs.
+
+Allows you to invite people without mods while having a modded game. Does not enable quickplay or prismatic trials.
+
+Have fun.
---
+++ UnmoddedClients.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+
+namespace UnmoddedClients {
+    [BepInPlugin("com.viseyth.ror2.unmoddedclients", "UnmoddedClient", "1.0.0")]
+    public class UnmoddedClients : BaseUnityPlugin {
+        private void Awake() {
+            On.RoR2.SteamworksLobbyManager.OnLobbyChanged += orig => {
+                orig();
+
+                var instance = RoR2.RoR2Application.instance;
+                if (instance == null)
+                    return;
+
+                var client = instance.steamworksClient;
+                if (client == null)
+                    return;
+                
+                RoR2.RoR2Application.instance.steamworksClient.Lobby.CurrentLobbyData.SetData("build_id", TextSerialization.ToStringInvariant(client.BuildId));
+            };
+        }
+    }
+}
