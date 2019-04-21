a2a369358466a96413adbe30ca47754e52824fa3
Vis'Eyth <viseyth_dev@protonmail.com>
2019-04-22 00:43:24 +0200

    Function and config update - v1.1.0

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 ## UnmoddedClients - BepInEx plugin for allowing unmodded clients to join
 
-##### PLEASE NOTE: This is very experimental. Please report any bugs.
+##### PLEASE NOTE: Due to the server sending the buildid to the client, i can't know what buildid the client is running on.
 
 Allows you to invite people without mods while having a modded game. Does not enable quickplay or prismatic trials.
 
-Have fun.
+---
+
+### Features
+#### Fully configurable in the config.
+Set your standard buildid to whatever you prefer:
+- `steam` - Automatically uses unmodded buildid.
+- `mod` - Automatically uses modded buildid.
+- Anything else - uses that, as a buildid.
+
+#### Multiple console functions:
+- `build_id_mod` - Sets your buildid to one representing a modded game. No arguments.
+- `build_id_steam` - Sets your buildid to one representing an unmodded game. No arguments.
+- `build_id_custom` - Use this to set your game to a custom buildid, for example to play with friends using a different version of the game. (That's not recommended by the way) One argument.
+
+---
+
+##### PLEASE NOTE: This is _still_ very experimental, and I'd love to hear feedback.
---
+++ UnmoddedClientsPlugin.cs
@@ -0,0 +1,98 @@
+﻿/**
+ * MIT License
+ *
+ * Copyright (c) 2019 Vis'Eyth (viseyth#3934)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using UnityEngine;
+
+namespace UnmoddedClients {
+    [BepInPlugin("com.viseyth.ror2.unmoddedclients", "UnmoddedClients", "1.1.0")]
+    public class UnmoddedClients : BaseUnityPlugin
+    {
+        private static string _buildId;
+        private static ConfigWrapper<string> _buildIdPreset;
+        
+        private void Awake() {
+            On.RoR2.SteamworksLobbyManager.OnLobbyChanged += orig => {
+                orig();
+                
+                RoR2Application.instance.steamworksClient.Lobby.CurrentLobbyData.SetData("build_id", _buildId);
+            };
+            On.RoR2.Console.Awake += (orig, self) => {
+                CommandHelper.RegisterCommands(self);
+                orig(self);
+            };
+            
+            _buildIdPreset = Config.Wrap("Game"
+                , "BuildIdPreset"
+                , "The preset Build-Id for the server at launch. Use mod, for modded"
+                + ", or steam for the real id. You can set any other value to set your server buildid to that value, too."
+                , "steam");
+            
+            switch (_buildIdPreset.Value)
+            {
+                case "mod":
+                    SetBuildIdMod(new ConCommandArgs());
+                    break;
+                case "steam":
+                    SetBuildIdSteam(new ConCommandArgs());
+                    break;
+                default:
+                    _buildId = _buildIdPreset.Value;
+                    break;
+            }
+                
+        }
+        
+        // ReSharper disable UnusedMember.Local UnusedParameter.Local
+        [ConCommand(commandName = "build_id_mod", flags = ConVarFlags.SenderMustBeServer, helpText = "Sets the buildid to MOD.")]
+        private static void SetBuildIdMod(ConCommandArgs args)
+        {
+            _buildId = "MOD";
+            Debug.Log($"BuildId set to {_buildId}");
+        }
+        [ConCommand(commandName = "build_id_steam", flags = ConVarFlags.SenderMustBeServer, helpText = "Sets buildid to the steam build id, as if your client was unmodded.")]
+        private static void SetBuildIdSteam(ConCommandArgs args)
+        {
+            var instance = RoR2Application.instance;
+            if (instance == null)
+                throw new ConCommandException("RoR2Application is null.");
+            var client = instance.steamworksClient;
+            if (client == null)
+                throw new ConCommandException("SteamworksClient is null");
+            
+            _buildId = TextSerialization.ToStringInvariant(client.BuildId);
+            Debug.Log($"BuildId set to {_buildId}");
+        }
+        [ConCommand(commandName = "build_id_custom", flags = ConVarFlags.SenderMustBeServer, helpText = "Sets the buildid to a custom value. One argument.")]
+        private static void SetBuildIdCustom(ConCommandArgs args)
+        {
+            args.CheckArgumentCount(1);
+            _buildId = args[0];
+            Debug.Log($"BuildId set to {_buildId}");
+        }
+        // ReSharper restore UnusedMember.Local UnusedParameter.Local
+    }
+}
